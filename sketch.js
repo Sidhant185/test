8a783e9df7526c821cc0d31fe3ef85b374441de5
Sidhant185 <sidhantpande222@gmail.com>
2021-01-01 22:52:19 -0800

    hello

--- sketch.js
+++ sketch.js
@@ -33,7 +33,7 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    box6 = new Box(900,4000,70,70);
     bird = new Bird(100,100);
 
 }
@@ -54,11 +54,11 @@ function draw(){
     box4.display();
     pig3.display();
     log3.display();
-
+    box6.display();
     box5.display();
     log4.display();
     log5.display();
-
+    
     bird.display();
     platform.display();
 }
